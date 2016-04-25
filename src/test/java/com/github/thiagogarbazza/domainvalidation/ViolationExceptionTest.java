2c2843c20764e0e0e8a99b3cd4185dee52bd1945
Thiago Garbazza <deinf.tpadua@bcb.gov.br>
2016-04-25 15:11:47 -0300

    Create test, and remove comments unnecessary

--- src/test/java/com/github/thiagogarbazza/domainvalidation/ViolationExceptionTest.java
+++ src/test/java/com/github/thiagogarbazza/domainvalidation/ViolationExceptionTest.java
@@ -1,11 +1,3 @@
-/*
- * Copyright (c) Banco Central do Brasil.
- *
- * Este software é confidencial e propriedade do Banco Central do Brasil.
- * Não é permitida sua distribuição ou divulgação do seu conteúdo sem
- * expressa autorização do Banco Central.
- * Este arquivo contém informações proprietárias.
- */
 package com.github.thiagogarbazza.domainvalidation;
 
 import org.junit.Test;
@@ -16,7 +8,7 @@ import static org.junit.Assert.assertEquals;
 public class ViolationExceptionTest {
 
     @Test
-    public void verifyMessageInException(){
+    public void verifyMessageInException() {
         final ViolationException violationException = new ViolationException(EMPTY_MAP);
         assertEquals("There was a breach in the restrictions of the entity.", violationException.getMessage());
     }
